2a9b35931924925782d60dd7161c9db2fd755054
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-10-16 15:09:06 -0400

    Add new API to test kubernetes deployment

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/items/test")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {
+        "item_id": item_id,
+        "q": q,
+        "post-commit": "This is a post commit API endpoint after Kubernetes deployment.",
+    }
